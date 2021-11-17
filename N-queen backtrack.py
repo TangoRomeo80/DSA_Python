26b751bdbdb73c1f5f441b161704161f4446c02f
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-11-17 08:53:22 +0600

    Updated N queen problem

--- N-queen backtrack.py
+++ N-queen backtrack.py
@@ -1,15 +1,9 @@
-global N
-N = 4
+
+def print_solution(board, n):
+    print('\n'.join([' '.join([str(item) for item in row])for row in board]))
 
 
-def print_solution(board):
-    for i in range(N):
-        for j in range(N):
-            print(board[i][j], end=' ')
-        print()
-
-
-def is_safe(board, row, col):
+def is_safe(board, row, col, n):
     for i in range(col):
         if board[row][i] == 1:
             return False
@@ -18,38 +12,37 @@ def is_safe(board, row, col):
         if board[i][j] == 1:
             return False
 
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
     return True
 
 
-def solver_func(board, col):
-    if col >= N:
+def solver_func(board, col, n):
+    if col >= n:
         return True
 
-    for i in range(N):
-        if is_safe(board, i, col):
+    for i in range(n):
+        if is_safe(board, i, col, n):
             board[i][col] = 1
-            if solver_func(board, col + 1):
+            if solver_func(board, col + 1, n):
                 return True
             board[i][col] = 0
 
     return False
 
 
-def solve():
-    board = [[0, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, 0]]
-
-    if not solver_func(board, 0):
+def solve(n):
+    board = [[0 for x in range(n)] for y in range(n)]
+    if not solver_func(board, 0, n):
         print("Solution does not exist")
         return False
 
-    print_solution(board)
+    print_solution(board, n)
     return True
 
-solve()
+
+N = int(input('Enter N value: '))
+solve(N)
+
