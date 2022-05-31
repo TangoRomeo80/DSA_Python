e8d027123e081f372f9a636c030e7b507fadf888
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-05-31 13:15:12 +0600

    added two_numbers_sum

---
+++ two_number_sum.py
@@ -0,0 +1,34 @@
+#O(n^2) time | O(1) space implementation
+def twoNumberSumNaive(array, targetSum):
+    for i in range(len(array)):
+        for j in range(i + 1, len(array)):
+            if array[i] + array[j] == targetSum:
+                return [array[i], array[j]]
+    return []
+
+#O(n) time | O(n) space implementation
+def twoNumberSumHash(array, targetSum):
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
+        else:
+            nums[num] = True
+    return []
+
+
+#O(n log n) time | O(1) space implementation
+def twoNumberSumSorted(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        else:
+            right -= 1
+    return []
