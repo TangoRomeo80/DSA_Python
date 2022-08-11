ea367498a08dca12afa9ebe313537c1c306f2bd8
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-11 17:08:50 +0600

    implement subarray sum square time

--- maximumSubarray_LC53.py
+++ maximumSubarray_LC53.py
@@ -4,3 +4,13 @@ def maxSubArrayCube(nums):
         for j in range(i, len(nums)):
             max_sum = max(max_sum, sum(nums[i:j+1]))
     return max_sum
+
+def maxSubArraySquare(nums):
+    max_sum = nums[0]
+    for i in range(len(nums)):
+        curr_sum = 0
+        for j in range(i, len(nums)):
+            curr_sum += nums[j]
+            max_sum = max(max_sum, curr_sum)
+    return max_sum
+
