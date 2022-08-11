227a77cadfedde606ff4c9543af779de585951e9
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-11 17:27:26 +0600

    add linear time solution for maximum subarray sum

--- maximumSubarray_LC53.py
+++ maximumSubarray_LC53.py
@@ -14,3 +14,13 @@ def maxSubArraySquare(nums):
             max_sum = max(max_sum, curr_sum)
     return max_sum
 
+def maxSubArray(nums):
+    max_sum = nums[0]
+    curr_sum = 0
+    for i in range(len(nums)):
+        curr_sum += nums[i]
+        max_sum = max(max_sum, curr_sum)
+        if curr_sum < 0:
+            curr_sum = 0
+    return max_sum
+
