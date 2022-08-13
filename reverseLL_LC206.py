d3a23bccb7473ad671f16c01e5763ca1d7f42c37
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-13 10:56:02 +0600

    add reverse linked list

---
+++ reverseLL_LC206.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(head):
+        if not head:
+            return None
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
+        
