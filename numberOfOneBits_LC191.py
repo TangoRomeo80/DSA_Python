544163c2c21aed36bdb79c3acc4c2ec95905884a
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-15 18:58:39 +0600

    implemented bitwise operation method for solving number of 1 bits

--- numberOfOneBits_LC191.py
+++ numberOfOneBits_LC191.py
@@ -1,5 +1,10 @@
 def hammingWeight(n):
 
-    return bin(n).count('1')
+    # return bin(n).count('1')
+    res = 0
+    while n:
+        n &= n - 1
+        res += 1
+    return res
 
 print(hammingWeight(7))
