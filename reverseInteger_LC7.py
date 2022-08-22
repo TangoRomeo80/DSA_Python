58455eab93230dd38d07fc1d7532d725bc347c31
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-22 15:32:55 +0600

    Create reverseInteger_LC7.py

---
+++ reverseInteger_LC7.py
@@ -0,0 +1,13 @@
+def reverse(x):
+    if x < 0:
+        x = -x
+        sign = -1
+    else:
+        sign = 1
+    res = 0
+    while x > 0:
+        res = res * 10 + x % 10
+        x = x // 10
+    if(res > 2**31):
+        return 0
+    return res * sign
