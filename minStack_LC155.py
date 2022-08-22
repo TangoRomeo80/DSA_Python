d4358105fd6a4c399776584921877d46d9aaed84
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-22 15:52:04 +0600

    Update minStack_LC155.py

--- minStack_LC155.py
+++ minStack_LC155.py
@@ -6,13 +6,13 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if not self.min_stack or val <= self.min_stack[-1]:
-            self.min_stack.append(val)
+        val = min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(val)
 
     def pop(self) -> None:
-        self.min_stack.pop()
         self.stack.pop()
-
+        self.min_stack.pop()
+        
     def top(self) -> int:
         return self.stack[-1]
 
