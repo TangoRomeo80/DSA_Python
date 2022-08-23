648bbc9bc36a3ff08f45eb23cfced1aefa8e79dc
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-23 09:27:07 +0600

    Update removeLinkedListElem_LC203.py

--- removeLinkedListElem_LC203.py
+++ removeLinkedListElem_LC203.py
@@ -5,18 +5,32 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if not head:
-            return head
-        while head and head.val == val:
-            head = head.next
-        if not head:
-            return head
-        cur = head
-        while cur.next:
-            if cur.next.val == val:
-                cur.next = cur.next.next
-            else:
-                cur = cur.next
+#         if not head:
+#             return head
+#         while head and head.val == val:
+#             head = head.next
+#         if not head:
+#             return head
+#         cur = head
+#         while cur.next:
+#             if cur.next.val == val:
+#                 cur.next = cur.next.next
+#             else:
+#                 cur = cur.next
         
-        return head
+#         return head
+
+        dummy = ListNode(next=head)
+        prev,cur = dummy, head
+        
+        while cur:
+            next = cur.next
             
+            if cur.val == val:
+                prev.next = next
+            else:
+                prev = cur
+                
+            cur = next
+        
+        return dummy.next
