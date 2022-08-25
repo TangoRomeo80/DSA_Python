cc575aeb8740b364ef18e0c3315ab37408e946c8
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-25 13:10:14 +0600

    Create lengthOfLastWord_LC58.py

---
+++ lengthOfLastWord_LC58.py
@@ -0,0 +1,12 @@
+def lengthOfLastWord(s):
+    if not s:
+        return 0
+    s = s.strip()
+    # return len(s.split(' ')[-1])
+    length = 0
+    for i in range(len(s) - 1, -1, -1):
+        if s[i] != ' ':
+            length += 1
+        else:
+            break
+        return length
