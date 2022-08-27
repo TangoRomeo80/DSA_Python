1a74dff2abab66515800867307586d57e853149a
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-27 21:31:49 +0600

    Create removeDuplicatesSortedList_LC.py

---
+++ removeDuplicatesSortedList_LC.py
@@ -0,0 +1,10 @@
+def deleteDuplicates(head):
+    if not head:
+        return head
+    cur = head
+    while cur.next:
+        if cur.val == cur.next.val:
+            cur.next = cur.next.next
+        else:
+            cur = cur.next
+    return head
