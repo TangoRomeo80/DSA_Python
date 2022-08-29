5d7744bcb1d41897ff75decc1f8888ea125d96c5
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-29 19:20:12 +0600

    Create soring.py

---
+++ soring.py
@@ -0,0 +1,25 @@
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+def selectionSort(arr):
+    for i in range(len(arr)):
+        minIndex = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[minIndex]:
+                minIndex = j
+        arr[i], arr[minIndex] = arr[minIndex], arr[i]
+    return arr
+
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
+    return arr
+
+print(bubbleSort([1,4,2,5,3,-1,-8,0,9,7,6]))
