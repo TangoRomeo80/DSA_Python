3d655d2e08bf30b53301a36e3cb791eaa4a81c96
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-30 15:05:06 +0600

    Update numberDisappeardInArray_LC448.py

--- numberDisappeardInArray_LC448.py
+++ numberDisappeardInArray_LC448.py
@@ -1,4 +1,10 @@
 def findDisappearedNumbers(nums):
+    for i in range(len(nums)):
+        index = abs(nums[i]) - 1
+        if nums[index] > 0:
+            nums[index] = -nums[index]
+    return [i + 1 for i in range(len(nums)) if nums[i] > 0]
+    
     # res = {}
     # out = []
     # for i in range(len(nums)):
@@ -10,4 +16,4 @@ def findDisappearedNumbers(nums):
         
     # return out
 
-    
+
