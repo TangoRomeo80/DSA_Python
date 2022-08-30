4092e20e68c4b959bc3a0a49acb761e6c22da8b8
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-08-30 14:40:01 +0600

    Create numberDisappeardInArray_LC448.py

---
+++ numberDisappeardInArray_LC448.py
@@ -0,0 +1,13 @@
+def findDisappearedNumbers(nums):
+    # res = {}
+    # out = []
+    # for i in range(len(nums)):
+    #     res[nums[i]] = 1
+
+    # for i in range(1, len(nums) + 1):
+    #     if i not in res:
+    #         out.append(i)
+        
+    # return out
+
+    
