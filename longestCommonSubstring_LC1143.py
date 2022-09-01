b490a7ea308998a69a25cb2c4a250c9f29657e55
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-01 14:17:12 +0600

    Update longestCommonSubstring_LC1143.py

--- longestCommonSubstring_LC1143.py
+++ longestCommonSubstring_LC1143.py
@@ -1,11 +1,27 @@
 def longestCommonSubsequence(text1, text2):
     dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
 
+    res = []
+
     for i in range(len(text1) - 1, -1, -1):
         for j in range(len(text2) - 1, -1, -1):
             if text1[i] == text2[j]:
-                dp[i][j] == 1 + dp[i + 1][j + 1]
+                dp[i][j] = 1 + dp[i + 1][j + 1]
             else:
                 dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
 
-    return dp[0][0]
+    i = 0
+    j = 0
+    while i < len(text1) and j < len(text2):
+        if text1[i] == text2[j]:
+            res.append(text1[i])
+            i += 1
+            j += 1
+        elif dp[i + 1][j] > dp[i][j + 1]:
+            i += 1
+        else:
+            j += 1
+    
+    return res
+
+print(longestCommonSubsequence('abab', 'baba'))
