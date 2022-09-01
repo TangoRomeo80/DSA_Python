94cd561278f1495b1edfeca7464adad6eaaa0432
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-01 12:04:46 +0600

    Create allPermutationOfList.py

---
+++ allPermutationOfList.py
@@ -0,0 +1,16 @@
+def permutation(lst):
+    if not lst:
+        return []
+    
+    if len(lst) == 1:
+        return [lst]
+    
+    res = []
+
+    for i in range(len(lst)):
+        for p in permutation(lst[:i] + lst[i+1:]):
+            res.append([lst[i]] + p)
+
+    return res
+
+print(permutation([1, 2, 3]))
