1d743c28564a79fc6e6fd7f10bd75373817ed6c3
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-03 23:41:51 +0600

    Update reverseString_LC344.py

--- reverseString_LC344.py
+++ reverseString_LC344.py
@@ -1,4 +1,27 @@
 def reverseString(s):
-    for i in range(len(s) // 2):
-        s[i], s[-i - 1] = s[-i -1], s[i]
-    return s
+    # for i in range(len(s) // 2):
+    #     s[i], s[-i - 1] = s[-i -1], s[i]
+    # return s
+
+    # stack = []
+
+    # for i in range(len(s)):
+    #     stack.append(s[i])
+    
+    # for i in range(len(s)):
+    #     s[i] = stack.pop()
+
+    # return s
+
+    # return s[::-1]
+
+    # return s.reverse()
+
+    def reverse(l, r):
+        if l < r:
+            s[l], s[r] = s[r], s[l]
+            reverse(l + 1, r - 1)
+
+    reverse(0, len(s) - 1)
+
+print(reverseString(['h','e','l','l','o']))
