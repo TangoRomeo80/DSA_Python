ca87f1e9d636d1a79e42296312a05a7671d483a7
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-04 14:23:22 +0600

    Create twoSum4_LC653.py

---
+++ twoSum4_LC653.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def findTarget(self, rootk):
+        d = {}
+        
+        def traverse(root, k):
+            if not root:
+                return False
+            if k - root.val in d:
+                return True
+            else:
+                d[root.val] = 1
+                return traverse(root.left, k) or traverse(root.right, k)
+                
+        return traverse(root, k)
