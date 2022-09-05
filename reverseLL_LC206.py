d12c868e6321b0152610a6d9f9ed76ed5cad374e
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-06 02:12:04 +0600

    Update reverseLL_LC206.py

--- reverseLL_LC206.py
+++ reverseLL_LC206.py
@@ -16,19 +16,34 @@ class Solution:
         # return prev
         
         #stack implementation
+        # if not head:
+        #     return None
+        
+        # s = []
+        # curr = head
+        # while curr:
+        #     s.append(curr.val)
+        #     curr = curr.next
+        # newHead = ListNode(s.pop())
+        # dummy = ListNode(next=newHead)
+        # while s:
+        #     newHead.next = ListNode(s.pop())
+        #     newHead = newHead.next
+            
+        # return dummy.next
+
+        #recursive implementation
+
         if not head:
             return None
-        
-        s = []
-        curr = head
-        while curr:
-            s.append(curr.val)
-            curr = curr.next
-        newHead = ListNode(s.pop())
-        dummy = ListNode(next=newHead)
-        while s:
-            newHead.next = ListNode(s.pop())
-            newHead = newHead.next
-            
-        return dummy.next
+
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return newHead
+
+
         
