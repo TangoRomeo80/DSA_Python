8462fafbd67ae8e7069f84d1e87230d311707f20
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-06 02:00:17 +0600

    Update climbingStairs_LC70.py

--- climbingStairs_LC70.py
+++ climbingStairs_LC70.py
@@ -1,12 +1,20 @@
 def climbStairs(n):
-    if n <= 3:
-        return n
-    n1, n2 = 2, 3
+    # if n <= 3:
+    #     return n
+    # n1, n2 = 2, 3
 
-    for i in range(4, n + 1):
-        temp = n1 + n2
-        n1 = n2
-        n2 = temp
-    return n2
+    # for i in range(4, n + 1):
+    #     temp = n1 + n2
+    #     n1 = n2
+    #     n2 = temp
+    # return n2
+
+    one, two = 1, 1
+    for i in range(n -1):
+        temp = one
+        one = one + two
+        two = temp
+            
+    return one
 
     
