5ddce87a6c78cd53d2817159fc54e366d66b40cf
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-06 14:57:33 +0600

    Update palindromeLL_LC234.py

--- palindromeLL_LC234.py
+++ palindromeLL_LC234.py
@@ -4,15 +4,38 @@ class ListNode:
         self.next = next
 class Solution:
     def isPalindrome(head):
-        if not head:
-            return True
-        stack = []
-        curr = head
-        while curr:
-            stack.append(curr.val)
-            curr = curr.next
-        while stack:
-            if stack.pop() != head.val:
+        # if not head:
+        #     return True
+        # stack = []
+        # curr = head
+        # while curr:
+        #     stack.append(curr.val)
+        #     curr = curr.next
+        # while stack:
+        #     if stack.pop() != head.val:
+        #         return False
+        #     head = head.next
+        # return True
+
+        fast, slow = head, head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+            
+        left, right = head, prev
+        
+        while right:
+            if left.val != right.val:
                 return False
-            head = head.next
+            left = left.next
+            right = right.next
+            
         return True
