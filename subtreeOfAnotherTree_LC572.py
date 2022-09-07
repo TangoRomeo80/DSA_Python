d4d29e690278392568837c0855c3407580e67420
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-07 11:11:20 +0600

    Update subtreeOfAnotherTree_LC572.py

--- subtreeOfAnotherTree_LC572.py
+++ subtreeOfAnotherTree_LC572.py
@@ -14,7 +14,7 @@ class Solution:
                 return False
             return self.isSameTree(root.left, subRoot.left) and self.isSameTree(root.right, subRoot.right)
         
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    def isSubtree(self, root, subRoot):
         if not root:
             return False
         if self.isSameTree(root, subRoot):
