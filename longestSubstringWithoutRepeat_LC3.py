3438b54f632b17febb3ce54fd3079d15df89ebed
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-09 10:40:13 +0600

    Update longestSubstringWithoutRepeat_LC3.py

--- longestSubstringWithoutRepeat_LC3.py
+++ longestSubstringWithoutRepeat_LC3.py
@@ -11,3 +11,19 @@ def lengthOfLongestSubstring(s):
     #             maxLen = max(maxLen, len(s[i:j]))
     # return maxLen
 
+    # O(n) solution
+    # Create a dictionary to store the last occurence of each character
+    charSet = set()
+    l = 0
+
+    res = 0
+    # Iterate through the string
+    for r in range(len(s)):
+        # If the character is not in the set, add it to the set
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        res = max(res, r-l+1)
+
+    return res
