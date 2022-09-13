865e28c2b6af2925f9c08d52f968b9879fe0475e
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-14 00:43:19 +0600

    Update maximumDepthOfBinaryTree_LC104.py

--- maximumDepthOfBinaryTree_LC104.py
+++ maximumDepthOfBinaryTree_LC104.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
+    def minDepth(self, root):
         if not root:
             return 0
         if not root.left and not root.right:
