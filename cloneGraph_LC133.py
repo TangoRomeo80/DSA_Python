e88a1ec6a53aded1253a4ba678b76cd027f6481c
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-13 14:19:59 +0600

    Update cloneGraph_LC133.py

--- cloneGraph_LC133.py
+++ cloneGraph_LC133.py
@@ -8,27 +8,44 @@ class Node:
 class Solution:
     def cloneGraph(self, node):
         #BFS solution
-        if not node:
-            return node
-        # create a new node
-        new_node = Node(node.val)
-        # create a dictionary to store the visited nodes
-        visited = {node: new_node}
-        # create a queue to store the nodes to be visited
-        queue = [node]
-        # start BFS traversal
-        while queue:
-            # pop a node from the front of the queue
-            n = queue.pop(0)
-            # iterate through all the neighbors of the node
-            for neighbor in n.neighbors:
-                if neighbor not in visited:
-                    # create a new neighbor
-                    new_neighbor = Node(neighbor.val)
-                    # add the new neighbor to the visited dictionary
-                    visited[neighbor] = new_neighbor
-                    # add the neighbor to the queue
-                    queue.append(neighbor)
-                # add the new neighbor to the neighbors of the new node
-                visited[n].neighbors.append(visited[neighbor])
-        return new_node
+        # if not node:
+        #     return node
+        # # create a new node
+        # new_node = Node(node.val)
+        # # create a dictionary to store the visited nodes
+        # visited = {node: new_node}
+        # # create a queue to store the nodes to be visited
+        # queue = [node]
+        # # start BFS traversal
+        # while queue:
+        #     # pop a node from the front of the queue
+        #     n = queue.pop(0)
+        #     # iterate through all the neighbors of the node
+        #     for neighbor in n.neighbors:
+        #         if neighbor not in visited:
+        #             # create a new neighbor
+        #             new_neighbor = Node(neighbor.val)
+        #             # add the new neighbor to the visited dictionary
+        #             visited[neighbor] = new_neighbor
+        #             # add the neighbor to the queue
+        #             queue.append(neighbor)
+        #         # add the new neighbor to the neighbors of the new node
+        #         visited[n].neighbors.append(visited[neighbor])
+        # return new_node
+
+        #DFS solution
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            new_node = Node(node.val)
+            oldToNew[node] = new_node
+            for neighbor in node.neighbors:
+                new_node.neighbors.append(dfs(neighbor))
+            return new_node
+
+        return dfs(node) if node else node
+
+        
+
