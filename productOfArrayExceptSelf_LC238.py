0c0e772a1c7f937db496f86dd8e29910f2e2d048
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-14 21:04:37 +0600

    Update productOfArrayExceptSelf_LC238.py

--- productOfArrayExceptSelf_LC238.py
+++ productOfArrayExceptSelf_LC238.py
@@ -1,13 +1,29 @@
 def productExceptSelf(nums):
     # Time: O(n) | Space: O(n)
 
-    prefix = [1] * len(nums)
-    suffix = [1] * len(nums)
+    # prefix = [1] * len(nums)
+    # suffix = [1] * len(nums)
+    # #calculate prefix array
+    # for i in range(1, len(nums)):
+    #     prefix[i] = prefix[i-1] * nums[i-1]
+    # #calculate suffix array
+    # for i in range(len(nums)-2, -1, -1):
+    #     suffix[i] = suffix[i+1] * nums[i+1]
+    # #calculate product of prefix and suffix
+    # return [prefix[i] * suffix[i] for i in range(len(nums))]
+
+    # Time: O(n) | Space: O(1)
+    res = [1] * len(nums)
     #calculate prefix array
-    for i in range(1, len(nums)):
-        prefix[i] = prefix[i-1] * nums[i-1]
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
     #calculate suffix array
-    for i in range(len(nums)-2, -1, -1):
-        suffix[i] = suffix[i+1] * nums[i+1]
-    #calculate product of prefix and suffix
-    return [prefix[i] * suffix[i] for i in range(len(nums))]
+    suffix = 1
+    for i in range(len(nums)-1, -1, -1):
+        res[i] *= suffix
+        suffix *= nums[i]
+
+    return res
+
