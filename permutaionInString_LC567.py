8da612fb82187c1ed2e2716089a0026308b54766
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-16 21:25:27 +0600

    Update permutaionInString_LC567.py

--- permutaionInString_LC567.py
+++ permutaionInString_LC567.py
@@ -10,31 +10,18 @@ class Solution:
         # return False
 
         #O(26.n)
-        def isAnagram(s, t):
-            if len(s) != len(t):
-                return False
-            d = {}
-            for i in s:
-                if i in d:
-                    d[i] += 1
-                else:
-                    d[i] = 1
-            for i in t:
-                if i in d:
-                    d[i] -= 1
-                else:
-                    d[i] = 1
-            for i in d:
-                if d[i] != 0:
-                    return False
-            return True
+        # c1 = Counter(s1)
 
-        if len(s1) > len(s2):
-            return False
-        for i in range(len(s2)-len(s1)+1):
-            if isAnagram(s1, s2[i:i+len(s1)]):
-                return True
-        return False
+        # for i in range(len(s2)-len(s1)+1):
+        #     if c1 == Counter(s2[i:i+len(s1)]):
+        #         return True
+        # return False
+
+        #O(n)
+        
+
+
+        
                     
             
 
