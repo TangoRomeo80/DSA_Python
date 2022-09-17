3993613fc8e0cbd480105fe0791fadc58283723f
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-09-17 10:08:02 +0600

    Update generateParentheses_LC22.py

--- generateParentheses_LC22.py
+++ generateParentheses_LC22.py
@@ -1,19 +1,57 @@
 class Solution:
     #solution using decision tree
-    def dfs(self, left, right, s, res):
-        if left == 0 and right == 0:
-            res.append(s)
-            return
-        if left > 0:
-            self.dfs(left-1, right, s+'(', res)
-        if right > left:
-            self.dfs(left, right-1, s+')', res)
+    # def dfs(self, left, right, s, res):
+    #     if left == 0 and right == 0:
+    #         res.append(s)
+    #         return
+    #     if left > 0:
+    #         self.dfs(left-1, right, s+'(', res)
+    #     if right > left:
+    #         self.dfs(left, right-1, s+')', res)
 
     def generateParenthesis(self, n: int) -> List[str]:
-        if n == 0:
-            return []
-        if n == 1:
-            return ["()"]
+        # if n == 0:
+        #     return []
+        # if n == 1:
+        #     return ["()"]
+        # res = []
+        # self.dfs(n, n, "", res)
+        # return res
+
+        #solution using stack
+        # if n == 0:
+        #     return []
+        # if n == 1:
+        #     return ["()"]
+        # res = []
+        # stack = [(n, n, "")]
+        # while stack:
+        #     left, right, s = stack.pop()
+        #     if left == 0 and right == 0:
+        #         res.append(s)
+        #     if left > 0:
+        #         stack.append((left-1, right, s+'('))
+        #     if right > left:
+        #         stack.append((left, right-1, s+')'))
+        # return res
+
+        #solution using backtracking
+        stack = []
         res = []
-        self.dfs(n, n, "", res)
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
         return res
