85ec637b74e1bc49b23d5d289a4a05bde107325e
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-02 13:25:14 +0600

    Create gasStation_LC134.py

---
+++ gasStation_LC134.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # start, end = len(gas) - 1, 0
+        # total = gas[start] - cost[start]
+
+        # while start >= end:
+        #     while total < 0 and start >= end:
+        #         start -= 1
+        #         total += gas[start] - cost[start]
+        #     if start == end:
+        #         return start
+        #     total += gas[end] - cost[end]
+        #     end += 1
+        # return -1
+
+        if sum(gas) < sum(cost):
+            return -1
+
+        total = 0
+        res = 0
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+            if total < 0:
+                res = i + 1
+                total = 0
+
+        return res
