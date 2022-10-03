eb58c8b5aac429891541c3180de72a7c5c0aef92
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-03 09:31:54 +0600

    Create validPOarenthesis_LC678.py

--- partitionLabels_LC763.py
+++ partitionLabels_LC763.py
@@ -1,21 +1,39 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        count = {}
+        # count = {}
+        # res = []
+        # i, length = 0, len(s)
+        # for j in range(length):
+        #     c = s[j]
+        #     count[c] = j
+
+        # curLen = 0
+        # goal = 0
+        # while i < length:
+        #     c = s[i]
+        #     goal = max(goal, count[c])
+        #     curLen += 1
+
+        #     if goal == i:
+        #         res.append(curLen)
+        #         curLen = 0
+        #     i += 1
+        # return res
+
+        lastIndex = {}
+
+        for i, c in enumerate(s):
+            lastIndex[c] = i
+
         res = []
-        i, length = 0, len(s)
-        for j in range(length):
-            c = s[j]
-            count[c] = j
+        size, end = 0, 0
 
-        curLen = 0
-        goal = 0
-        while i < length:
-            c = s[i]
-            goal = max(goal, count[c])
-            curLen += 1
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, lastIndex[c])
 
-            if goal == i:
-                res.append(curLen)
-                curLen = 0
-            i += 1
+            if i == end:
+                res.append(size)
+                size = 0
+
         return res
