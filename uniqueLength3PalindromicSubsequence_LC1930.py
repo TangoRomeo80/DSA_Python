c5cd54375566953387fdbe153f1b564c079fe527
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-09 10:12:29 +0600

    Update uniqueLength3PalindromicSubsequence_LC1930.py

--- uniqueLength3PalindromicSubsequence_LC1930.py
+++ uniqueLength3PalindromicSubsequence_LC1930.py
@@ -2,20 +2,27 @@ import collections
 
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        res = set()
-        left = set()
-        right = collections.Counter(s)
+        count = 0
+        for c in set(s):
+            i, j = s.find(c), s.rfind(c)
+            count += len(set(s[i+1:j]))
+        return count
 
-        for i in range(len(s)):
-            right[s[i]] -= 1
-            if right[s[i]] == 0:
-                right.pop(s[i])
+        # res = set()
+        # left = set()
+        # right = collections.Counter(s)
+
+        # for i in range(len(s)):
+        #     right[s[i]] -= 1
+        #     if right[s[i]] == 0:
+        #         right.pop(s[i])
             
-            for j in range(26):
-                c = chr(ord('a') + j)
-                if c in left and c in right:
-                    res.add((s[i], c))
+        #     for j in range(26):
+        #         c = chr(ord('a') + j)
+        #         if c in left and c in right:
+        #             res.add((s[i], c))
             
-            left.add(s[i])
+        #     left.add(s[i])
     
-        return len(res)
+        # return len(res)
+
