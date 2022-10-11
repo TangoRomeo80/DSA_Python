93805ce0bdcd48a1db7994184a01bbdd756381d0
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-11 11:15:40 +0600

    Create findAllAnagramsInStr_LC438.py

---
+++ findAllAnagramsInStr_LC438.py
@@ -0,0 +1,19 @@
+import collections
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s) < len(p):
+            return []
+        result = []
+        pCounter = collections.Counter(p)
+        sCounter = collections.Counter(s[:len(p)-1])
+        for i in range(len(p)-1, len(s)):
+            sCounter[s[i]] += 1
+            if sCounter == pCounter:
+                result.append(i-len(p)+1)
+            
+            sCounter[s[i-len(p)+1]] -= 1
+            if sCounter[s[i-len(p)+1]] == 0:
+                del sCounter[s[i-len(p)+1]]
+        return result
+        
