d3f93bf7547689d62b2df57fc6c41ec62a252337
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-17 14:59:25 +0600

    Update numberOfSubsequencesSatisfyGivenSum_LC1498.py

--- numberOfSubsequencesSatisfyGivenSum_LC1498.py
+++ numberOfSubsequencesSatisfyGivenSum_LC1498.py
@@ -1,12 +1,40 @@
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
+        #gets TLE
+        # nums.sort()
+        # res = 0
+        # l, r = 0, len(nums) - 1
+        # while l <= r:
+        #     if nums[l] + nums[r] > target:
+        #         r -= 1
+        #     else:
+        #         res += 2 ** (r - l)
+        #         l += 1
+        # return res % (10 ** 9 + 7)
+
+        #TLE as well
+        # nums.sort()
+        # res = 0
+        # r = len(nums) - 1
+        # for i in range(len(nums)):
+        #     while (nums[i] + nums[r]) > target and i <= r:
+        #         r -= 1
+        #     if i <= r:
+        #         res += 2 ** (r - i)
+        #         res %= (10 ** 9 + 7)  
+        # return res % (10 ** 9 + 7) 
+
+        #AC
         nums.sort()
-        res = 0
         l, r = 0, len(nums) - 1
+        res = 0
+        mod = 10**9 + 7
         while l <= r:
             if nums[l] + nums[r] > target:
                 r -= 1
             else:
-                res += 2 ** (r - l)
+                res += pow(2, r - l, mod)
                 l += 1
-        return res % (10 ** 9 + 7)
+        return res % mod   
+            
+        
