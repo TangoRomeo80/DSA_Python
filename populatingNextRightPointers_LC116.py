93115e11eeff1cda36bcc8af243ecbb1eb3c838d
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-24 11:09:03 +0600

    Update populatingNextRightPointers_LC116.py

--- populatingNextRightPointers_LC116.py
+++ populatingNextRightPointers_LC116.py
@@ -11,17 +11,29 @@ class Node:
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         #BFS solution
-        if not root:
-            return None
-        queue = [root]
-        while queue:
-            size = len(queue)
-            for i in range(size):
-                node = queue.pop(0)
-                if i < size - 1:
-                    node.next = queue[0]
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+        # if not root:
+        #     return None
+        # queue = [root]
+        # while queue:
+        #     size = len(queue)
+        #     for i in range(size):
+        #         node = queue.pop(0)
+        #         if i < size - 1:
+        #             node.next = queue[0]
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        # return root
+
+        #DFS solution
+        cur, nxt = root, root.left if root else None
+
+        while cur and nxt:
+            cur.left.next = cur.right
+            cur.right.next = cur.next.left if cur.next else None
+            cur = cur.next
+            if not cur:
+                cur = nxt
+                nxt = cur.left
         return root
