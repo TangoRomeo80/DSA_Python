bfe164d5f16467b739b732d92ed68e912f943fbd
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-10-26 09:45:48 +0600

    Create partitionList_LC86.py

---
+++ partitionList_LC86.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        left, right = ListNode(), ListNode()
+        ltail, rtail = left, right
+
+        while head:
+            if head.val < x:
+                ltail.next = head
+                ltail = ltail.next
+            else:
+                rtail.next = head
+                rtail = rtail.next
+            head = head.next
+        
+        rtail.next = None
+        ltail.next = right.next
+        return left.next
