93aba3a1ced7622ffb68c0490e2e5151b244fd3c
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-11-15 10:10:17 +0600

    Update rangeSumQuerryImmutable_LC303.py

--- rangeSumQuerryImmutable_LC303.py
+++ rangeSumQuerryImmutable_LC303.py
@@ -1,11 +1,23 @@
 class NumArray:
 
+    # def __init__(self, nums: List[int]):
+    #     self.nums = nums
+        
+    # def sumRange(self, left: int, right: int) -> int:
+    #     return sum(self.nums[left:right+1])
+
+
     def __init__(self, nums: List[int]):
-        self.nums = nums
-        
+        self.nums=nums
+        self.pre=nums
+        for i in range(len(self.pre)-1):
+            self.pre[i+1]+=self.pre[i]
+
     def sumRange(self, left: int, right: int) -> int:
-        return sum(self.nums[left:right+1])
-        
+        if left:
+            return self.pre[right]-self.pre[left-1]
+        else:
+            return self.pre[right]
 
 
 # Your NumArray object will be instantiated and called as such:
