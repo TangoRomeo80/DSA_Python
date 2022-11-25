d69f9b64ef0f4302f7f345603336cc855ef7f3ac
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-11-25 10:08:49 +0600

    Create repeatedSubstringPattern_LC459.py

---
+++ repeatedSubstringPattern_LC459.py
@@ -0,0 +1,10 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        # O(n^2) time, O(1) space
+        for i in range(1, len(s)):
+            if len(s) % i == 0:
+                if s[:i] * (len(s) // i) == s:
+                    return True
+        return False
+
+        
