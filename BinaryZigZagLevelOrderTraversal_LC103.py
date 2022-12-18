1c987e5dde0f0e41693f327f6616e31b154ef231
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-12-18 09:42:07 +0600

    Create BinaryZigZagLevelOrderTraversal_LC103.py

---
+++ BinaryZigZagLevelOrderTraversal_LC103.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        result = []
+        queue = deque([root])
+        level = 0
+        while queue:
+            level += 1
+            level_size = len(queue)
+            current_level = []
+            for _ in range(level_size):
+                current_node = queue.popleft()
+                current_level.append(current_node.val)
+                if current_node.left:
+                    queue.append(current_node.left)
+                if current_node.right:
+                    queue.append(current_node.right)
+            if level % 2 == 0:
+                result.append(current_level[::-1])
+            else:
+                result.append(current_level)
+        return result
