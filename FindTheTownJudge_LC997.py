ab3248e454fef34b3777970666787802d78c7183
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-12-22 10:06:48 +0600

    Create FindTheTownJudge_LC997.py

---
+++ FindTheTownJudge_LC997.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trusted = [0] * (n+1)
+        for i, j in trust:
+            trusted[i] -= 1
+            trusted[j] += 1
+        for i in range(1, n+1):
+            if trusted[i] == n-1:
+                return i
+        return -1
