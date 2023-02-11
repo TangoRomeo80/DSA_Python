b29cb16add4500aad3e300ea51c2245317343ac3
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-02-11 11:27:48 +0600

    Create Matrix_LC542.py

---
+++ Matrix_LC542.py
@@ -0,0 +1,20 @@
+import collections
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        q = collections.deque()
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                else:
+                    mat[i][j] = -1
+        while q:
+            i, j = q.popleft()
+            for x, y in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)):
+                if 0 <= x < m and 0 <= y < n and mat[x][y] == -1:
+                    mat[x][y] = mat[i][j] + 1
+                    q.append((x, y))
+        return mat
