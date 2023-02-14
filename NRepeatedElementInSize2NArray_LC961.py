12ccc59882c1490c9435402edea1964467537d04
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-02-14 10:50:38 +0600

    Create NRepeatedElementInSize2NArray_LC961.py

---
+++ NRepeatedElementInSize2NArray_LC961.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        n = len(nums) // 2
+        c = Counter(nums)
+        for k, v in c.items():
+            if v == n:
+                return k
+            
+    
