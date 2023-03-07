e161c51f41cea5614b19ec28a2337385a11b29a1
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-03-07 09:07:42 +0600

    Create DeleteOperationsForTwoStrings_LC583.py

---
+++ DeleteOperationsForTwoStrings_LC583.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        def lcs(word1, word2):
+            m, n = len(word1), len(word2)
+            dp = [[0] * (n + 1) for _ in range(m + 1)]
+            for i in range(m):
+                for j in range(n):
+                    if word1[i] == word2[j]:
+                        dp[i + 1][j + 1] = dp[i][j] + 1
+                    else:
+                        dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+            return dp[m][n]
+        return len(word1) + len(word2) - 2 * lcs(word1, word2)
+
+        
