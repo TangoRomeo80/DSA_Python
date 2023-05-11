2a5dce6936e6a00463af947e82c31ec5aad86d2d
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-05-11 10:37:09 +0600

    Update ConcatenationOfArray_LC1929.py

--- ConcatenationOfArray_LC1929.py
+++ ConcatenationOfArray_LC1929.py
@@ -1,3 +1,10 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        return nums + nums
+        # return nums + nums
+        ans = []
+
+        for i in range(2):
+            for n in nums:
+                ans.append(n)
+        
+        return ans
