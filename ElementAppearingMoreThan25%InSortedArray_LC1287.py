82cf1c213fa0980b17f0931abcdc1c45cd3a17ff
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-05-18 11:12:21 +0600

    Create ElementAppearingMoreThan25%InSortedArray_LC1287.py

---
+++ ElementAppearingMoreThan25%InSortedArray_LC1287.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n == 1:
+            return arr[0]
+        count = 1
+        for i in range(1, n):
+            if arr[i] == arr[i-1]:
+                count += 1
+                if count > n/4:
+                    return arr[i]
+            else:
+                count = 1
+        return -1
