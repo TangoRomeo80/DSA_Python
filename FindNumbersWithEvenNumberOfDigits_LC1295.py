cf00efc4df06fc592247527bf3318e3dee685bab
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-05-20 15:35:53 +0600

    Update FindNumbersWithEvenNumberOfDigits_LC1295.py

--- FindNumbersWithEvenNumberOfDigits_LC1295.py
+++ FindNumbersWithEvenNumberOfDigits_LC1295.py
@@ -1,7 +1,16 @@
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        count = 0
+        # count = 0
+        # for num in nums:
+        #     if len(str(num)) % 2 == 0:
+        #         count += 1
+        # return count
+
+        result = 0
+        
         for num in nums:
-            if len(str(num)) % 2 == 0:
-                count += 1
-        return count
+            num_digits = floor(log10(num)) + 1
+            if num_digits % 2 == 0:
+                result += 1
+                
+        return result
