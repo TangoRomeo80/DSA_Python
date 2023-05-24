56f7bdc6c9f7470568b7a8dab4f236b41bdbacfb
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-05-24 10:33:49 +0600

    Update ConvertIntegertotheSumofTwoNo-ZeroIntegers_LC1317.py

--- ConvertIntegertotheSumofTwoNo-ZeroIntegers_LC1317.py
+++ ConvertIntegertotheSumofTwoNo-ZeroIntegers_LC1317.py
@@ -1,7 +1,18 @@
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
-        for i in range(1, n):
-            if '0' not in str(i) and '0' not in str(n-i):
-                return [i, n-i]
+        # for i in range(1, n):
+        #     if '0' not in str(i) and '0' not in str(n-i):
+        #         return [i, n-i]
+
+        def check(num):
+            while num>0:
+                if num%10==0:
+                    return False
+                num//=10
+            return True
+        for i in range(1,n):
+            t=n-i
+            if check(t) and check(i):
+                return [i,t]
 
                 
